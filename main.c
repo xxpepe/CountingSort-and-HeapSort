fe5c609b8b002b00e29a0402f180aba30f5aa1f0
Gabriel <josegabrielpe@hotmail.com>
2018-08-29 22:56:52 -0300

    Quase

---
+++ main.c
@@ -0,0 +1,129 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define NUM_ELEMENTOS 5
+//#define TAM_HEAP 5
+
+int TAM_HEAP = 5; 
+
+void swap(int *x, int *y){ 
+   int aux;
+   aux=*x;
+   *x=*y;
+   *y=aux;
+}
+
+void CountingSort(int *A, int *B, int k, int n){ // k = maior valor do array
+    int i = 0, j=0;
+    int C[k];
+
+    for (i=0 ; i<=k ; i++){
+        C[i] = 0;
+    }
+
+    for (j=0 ; j<n ; j++){
+        C[A[j]] = C[A[j]] + 1; //Acrescentando em C quantas vezes aparece os numeros do array A
+    }
+    
+    for (i=1 ; i<=k ; i++){
+        C[i] = C[i] + C[i-1]; //Ve quantos elementos tem antes dele 
+    }
+
+    for (j=(n-1); j>=0 ; j--){
+        B[C[A[j]]-1] = A[j];
+        C[A[j]] = C[A[j]] - 1;
+    }
+
+} 
+
+void MaxHeapfy(int *A, int i){
+    int esquerdo = (2*i)+1; 
+    int direito = (2*i)+2;
+    int maior = i;
+    
+    //TAM_HEAP = sizeof(A);
+
+    if(esquerdo<=TAM_HEAP && A[esquerdo]>A[maior]){
+        maior = esquerdo;
+    }
+
+    if(direito<=TAM_HEAP && A[direito]>A[maior]){
+        maior = direito;
+    }
+
+    if(maior!=i){
+        swap(&A[i],&A[maior]);
+        MaxHeapfy(A,maior);
+    }
+}
+
+void BuildMaxHeap(int *A){
+    int i = 0;
+    //TAM_HEAP = sizeof(A);
+    for (i = (TAM_HEAP/2) - 1; i >=0; i--){
+        MaxHeapfy(A,i);
+    }
+}
+
+void HeapSort(int *A){
+    BuildMaxHeap(A);
+    int i = 0;
+    for (i = TAM_HEAP - 1; i>=0 ; i--){
+        swap(&A[0], &A[i]);
+        TAM_HEAP = TAM_HEAP -1;
+        MaxHeapfy(A,0);
+    }
+
+}
+
+//Funcao para imprimir o array
+void Imprime(int *array){
+    int k=0;
+    
+    for (k ; k<5 ; k++){
+        printf(" |%d| ", array[k]);
+    }
+    puts("");
+}
+
+void Imprime2(int *array){
+    int k=0;
+    
+    for (k ; k<6 ; k++){
+        printf(" |%d| ", array[k]);
+    }
+    puts("");
+}
+
+int main(){
+    
+    int i = 0;
+    int array[NUM_ELEMENTOS] = {21,2,34,17,33}; 
+    int array2[NUM_ELEMENTOS];
+
+    for (i=0 ; i<NUM_ELEMENTOS ; i++){
+        array2[i] = 0;
+    }
+
+    //int array[NUM_ELEMENTOS] = {50,40,30,20,10};
+    printf("\nArray 1 antes da troca:\n");
+    Imprime(array);
+
+    puts("Organizando o array 1 com o CountingSort:");
+    CountingSort(array, array2, 34, 5 );
+    Imprime(array2);
+
+    //int array3[NUM_ELEMENTOS] = {21,2,34,17,33}; 
+    //int array3[NUM_ELEMENTOS] = {1, 15, 2, 1, 89}; 
+    //int array3[NUM_ELEMENTOS] = {50,40,30,20,10};
+    int array3[NUM_ELEMENTOS + 1] = {1, 15, 2, 47, 89, 25}; 
+
+    printf("\nArray 2 antes da troca:\n");
+    Imprime2(array3);
+
+    puts("Organizando o array 2 com o HeapSort:");
+    HeapSort(array3);
+    Imprime2(array3);
+    
+    return 0;
+}
